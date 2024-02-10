6dc29b0f8f3f7ef2e790b96a0a34936418831c6e
Yehyeok Bang <qkddpgur318@gmail.com>
2024-02-11 02:23:43 +0900

    feat: 댓글, 대댓글 정렬 기능 구현
    - 댓글과 대댓글을 기준에 맞게 정렬 후 반환합니다.

--- pkg/server/handler/post_handler.go
+++ pkg/server/handler/post_handler.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"context"
 	"strconv"
-	"time"
 
 	pb "github.com/YehyeokBang/Simple-SNS/pkg/api/v1/post"
 	"github.com/YehyeokBang/Simple-SNS/pkg/auth"
@@ -56,42 +55,37 @@ func (h *PostHandler) WritePost(ctx context.Context, req *pb.WritePostRequest) (
 	}, nil
 }
 
+func Paginate(req *pb.GetPostsRequest) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if req.GetPage() != 0 {
+			offset := int((req.GetPage() - 1) * req.GetLimit())
+			return db.Offset(offset).Limit(int(req.GetLimit()))
+		}
+		return db
+	}
+}
+
 func (h *PostHandler) GetPosts(ctx context.Context, req *pb.GetPostsRequest) (*pb.GetPostsResponse, error) {
-	type Result struct {
-		PostID    uint
-		UserName  string
-		Title     string
-		CreatedAt time.Time
-		UpdatedAt time.Time
-		Count     int64
-	}
-
-	var results []Result
-	query := h.DB.Model(&db.Post{}).
-		Select("posts.id as post_id, users.name as user_name, posts.title, posts.created_at, posts.updated_at, count(comments.id) as count").
-		Joins("left join users on users.id = posts.user_id").
-		Joins("left join comments on comments.post_id = posts.id").
-		Group("posts.id, users.name, posts.content, posts.created_at, posts.updated_at").
-		Order("posts.updated_at desc")
-
-	if req.GetPage() != 0 {
-		query = query.Limit(int(req.GetLimit())).Offset(int((req.GetPage() - 1) * req.GetLimit()))
-	}
-	result := query.Find(&results)
+	var posts []db.Post
+	result := h.DB.Scopes(Paginate(req)).
+		Preload("User").
+		Preload("Comments").
+		Find(&posts)
 
 	if result.Error != nil {
 		return nil, status.Error(codes.Internal, "failed to get posts")
 	}
 
 	var pbPosts []*pb.PostSummary
-	for _, result := range results {
+	for _, post := range posts {
+		commentCount := len(post.Comments)
 		pbPosts = append(pbPosts, &pb.PostSummary{
-			Id:           uint32(result.PostID),
-			UserName:     result.UserName,
-			Title:        result.Title,
-			CommentCount: uint32(result.Count),
-			CreatedAt:    timestamppb.New(result.CreatedAt),
-			UpdatedAt:    timestamppb.New(result.UpdatedAt),
+			Id:           uint32(post.ID),
+			UserName:     post.User.Name,
+			Title:        post.Title,
+			CommentCount: uint32(commentCount),
+			CreatedAt:    timestamppb.New(post.CreatedAt),
+			UpdatedAt:    timestamppb.New(post.UpdatedAt),
 		})
 	}
 
@@ -100,9 +94,34 @@ func (h *PostHandler) GetPosts(ctx context.Context, req *pb.GetPostsRequest) (*p
 	}, nil
 }
 
+func sortComments(comments []*pb.Comment) []*pb.Comment {
+	normalComments := make([]*pb.Comment, 0)
+	replyComments := make(map[uint32][]*pb.Comment)
+
+	for _, comment := range comments {
+		if comment.HasParent {
+			replyComments[comment.ParentId] = append(replyComments[comment.ParentId], comment)
+		} else {
+			normalComments = append(normalComments, comment)
+		}
+	}
+
+	sortedComments := make([]*pb.Comment, 0)
+	for _, comment := range normalComments {
+		sortedComments = append(sortedComments, comment)
+		sortedComments = append(sortedComments, replyComments[comment.Id]...)
+	}
+
+	return sortedComments
+}
+
 func (h *PostHandler) GetPostById(ctx context.Context, req *pb.GetPostByIdRequest) (*pb.GetPostByIdResponse, error) {
 	var post db.Post
-	result := h.DB.Joins("User").Preload("Comments").Preload("Comments.User").First(&post, req.GetId())
+	result := h.DB.Joins("User").
+		Preload("Comments").
+		Preload("Comments.User").
+		First(&post, req.GetId())
+
 	if result.Error != nil {
 		return nil, status.Error(codes.NotFound, "post is not exists")
 	}
@@ -133,7 +152,7 @@ func (h *PostHandler) GetPostById(ctx context.Context, req *pb.GetPostByIdReques
 			UserName:  post.User.Name,
 			Title:     post.Title,
 			Content:   post.Content,
-			Comments:  pbComments,
+			Comments:  sortComments(pbComments),
 			CreatedAt: timestamppb.New(post.CreatedAt),
 			UpdatedAt: timestamppb.New(post.UpdatedAt),
 		},
